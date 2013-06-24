cb337d11b1e2f5dbd9f303501afa5197284f4633
adriencatel <adrien.catel@everytime-technologies.com>
2013-06-24 14:35:47 +0200

    new config.xml

--- gen/com/everytime/R.java
+++ gen/com/everytime/R.java
@@ -66,7 +66,8 @@ public final class R {
         public static final int AppTheme=0x7f070001;
     }
     public static final class xml {
-        public static final int phonegap=0x7f040000;
-        public static final int plugins=0x7f040001;
+        public static final int config=0x7f040000;
+        public static final int phonegap=0x7f040001;
+        public static final int plugins=0x7f040002;
     }
 }
